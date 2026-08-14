0401d165331825c2f5fea4dab1c57621fbd4a8bb
João Marcos <164503476+Jmarcosls@users.noreply.github.com>
2026-08-14 11:02:01 -0300

    Update and rename notificações.py to notificacoes.py
    Foi adicionado todas algumas das notificações que o sistema irá imprimir para o usuário durante o uso do sistema.

---
+++ notificacoes.py
@@ -0,0 +1,45 @@
+def horario_indisponivel():
+    print("\n====================================")
+    print("ERRO: horário indisponível.")
+    print("Escolha outro horário.")
+    print("====================================\n")
+
+
+def data_invalida():
+    print("\n====================================")
+    print("ERRO: data inválida.")
+    print("As reservas são permitidas apenas")
+    print("a partir da data atual.")
+    print("====================================\n")
+
+
+def domingo_indisponivel():
+    print("\n====================================")
+    print("ERRO: não é permitido reservar")
+    print("espaços aos domingos.")
+    print("====================================\n")
+
+
+def local_reservado():
+    print("\n====================================")
+    print("ERRO: o local já está reservado")
+    print("para a data e o horário selecionados.")
+    print("====================================\n")
+
+
+def reserva_realizada():
+    print("\n====================================")
+    print("Reserva realizada com sucesso!")
+    print("====================================\n")
+
+
+def reserva_cancelada():
+    print("\n====================================")
+    print("Reserva cancelada com sucesso!")
+    print("====================================\n")
+
+
+def opcao_invalida():
+    print("\n====================================")
+    print("ERRO: opção inválida.")
+    print("====================================\n")
