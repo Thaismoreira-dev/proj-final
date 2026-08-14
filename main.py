7d4f4633159cf9a070d480885b0325430a94c8a3
João Marcos <164503476+Jmarcosls@users.noreply.github.com>
2026-08-14 11:04:27 -0300

    Update main.py
    Importei as funções do arquivo de notificações.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+from notificacoes import (
+    horario_indisponivel,
+    data_invalida,
+    domingo_indisponivel,
+    local_reservado,
+    reserva_realizada,
+    reserva_cancelada,
+    opcao_invalida
+)
