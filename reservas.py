534a1ee189746a303fc0e48a6997e40cf8a058af
Gabriel Cavalcante Cavrioni <gabrielcpereira4040@gmail.com>
2026-08-14 10:18:16 -0300

    Update reservas.py
    Eu estou testando para ver se fico como contribuidor

--- reservas.py
+++ reservas.py
@@ -1,1 +1,41 @@
+espacos = []
 
+def cadastrar_espaco():
+    id_espaco = input("ID do espaço: ")
+    nome = input("Nome do espaço: ")
+    capacidade = int(input("Capacidade: "))
+    projetor = input("Possui projetor? (s/n): ")
+    tem_projetor = projetor == "s"
+
+    espacos.append({"id_espaco": id_espaco, "nome": nome, "capacidade": capacidade, "tem_projetor": tem_projetor})
+    print("Espaço cadastrado com sucesso!")
+
+
+def listar_espacos():
+    if not espacos:
+        print("Não tem nenhum espaço cadastrado")
+        return
+
+    print("Espaçõses cadastrados: ")
+    for espaco in espacos:
+        print(f"ID: {espaco['id_espaco']}",
+              f"Nome: {espaco['nome']}",
+              f"Capacidade: {espaco['capacidade']}",
+              f"Projetor: {espaco['tem_projetor']}")
+
+import escopo
+
+while True:
+    print("1 - Cadastrar espaço")
+    print("2 - Listar espaços")
+    print("3 - Sair")
+
+    opcao = input("Escolha uma opção: ")
+    if opcao == "1":
+        escopo.cadastrar_espaco()
+    elif opcao == "2":
+        escopo.listar_espacos()
+    elif opcao == "3":
+        break
+    else:
+        print("Opção inválida!")
