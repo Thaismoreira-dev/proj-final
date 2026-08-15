9ea53f1a1d00c710bf134241b125f81fbb3614f2
Thaismoreira-dev <thaissyt13@gmail.com>
2026-08-15 09:25:01 -0300

    Implement time slot reservation system
    Added a reservation system for time slots with user input.

--- horarios.py
+++ horarios.py
@@ -1,3 +1,38 @@
-HORARIOS = [
-    '8h'
-]
+
+horario=[
+ "[1] Manhã",                                                  
+ "[2] Tarde",                                                  
+ "[3] Noite",                                                  
+ ]       
+                                           
+opcao=int(input('''
+╔═════════════════════════════════════════════════════════════════════╗
+║           SISTEMA DE RESERVA DE ESPAÇOS FÍSICOS - CAMPUS            ║
+║                                                                     ║
+╠═════════════════════════════════════════════════════════════════════╣
+║ [1] Manhã                                                           ║
+║ [2] Tarde                                                           ║
+║ [3] Noite                                                           ║
+╚═════════════════════════════════════════════════════════════════════╝
+Digite o número referente ao horário que deseja reservar:'''))
+
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
+def reservar_horario (opcao):
+    if opcao >=1 and opcao<=3:
+        escolha = opcao - 1
+        print("Seu horário foi reservado com sucesso!")
+        print(horario[escolha])
+
+        arquivo=open ("reserva.txt", "a", encoding='utf-8')
+        arquivo
+
+    else:
+        print("Opção Inválida")
+
+
+reservar_horario(opcao)
+
