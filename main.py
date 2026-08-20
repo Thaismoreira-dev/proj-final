69f35e67c2d8ce7fe9238e63dfa4c0843e538df3
Gabriel Cavalcante <gabrielcpereira4040@gmail.com>
2026-08-20 19:22:20 -0300

    Update main.py
    Adicionei minha parte depois do João Marcos

--- main.py
+++ main.py
@@ -16,3 +16,15 @@ def listar_espacos_disponiveis():
 
     for espaco in retornar_espacos_disponiveis():
         print(f"{espaco['codigo']} - {espaco['nome']} ({espaco['tipo']})")
+
+#A parte que ficou comigo Gabriel Cavalcante
+import reservas
+codigo = input("Código do espaço: ")
+responsavel = input("Responsável: ")
+reservas.criar_reserva(codigo, responsavel)
+reservas.listar_reservas()
+resultado = reservas.consultar_reserva(codigo)
+if resultado:
+    print("Reserva encontrada!")
+else:
+    print("Reserva não encontrada!")
